3c0058901be0b7720a6bc22fc1c10193d1a44e0c
liuweizhong <978959516@qq.com>
2024-02-19 22:32:08 +0800

    sa-token配置调整

--- matrix-admin/src/main/java/com/liu/matrixadmin/config/SaTokenConfigure.java
+++ matrix-admin/src/main/java/com/liu/matrixadmin/config/SaTokenConfigure.java
@@ -30,9 +30,12 @@ public class SaTokenConfigure implements WebMvcConfigurer {
         registry.addInterceptor(new SaInterceptor(handler -> {
             // 指定一条 match 规则
             SaRouter.match("/**")    // 拦截的 path 列表，可以写多个
-                    .notMatch("/user/login", "/swagger-ui.html", "/openApi/**")      // 排除掉的 path 列表，可以写多个
+                    // 开放登录接口
+                    .notMatch("/user/login")
                     // 开放 swagger
                     .notMatch("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
+                    // 开放自定义接口
+                    .notMatch("/openApi/**")
                     .check(r -> StpUtil.checkLogin());        // 要执行的校验动作，可以写完整的 lambda 表达式
         })).addPathPatterns("/**");
     }
