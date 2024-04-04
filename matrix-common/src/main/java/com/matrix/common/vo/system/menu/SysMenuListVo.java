605cfd838f04b7dbe12fb468acd90cd452198f05
liuweizhong <978959516@qq.com>
2024-04-04 18:42:06 +0800

    菜单查询功能修改

--- matrix-common/src/main/java/com/matrix/common/vo/system/menu/SysMenuListVo.java
+++ matrix-common/src/main/java/com/matrix/common/vo/system/menu/SysMenuListVo.java
@@ -1,7 +1,9 @@
 package com.matrix.common.vo.system.menu;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -41,7 +43,9 @@ public class SysMenuListVo {
     @Schema(description = "是否显示在菜单栏,0：不显示，1：显示")
     private Boolean hidden;
 
-    @Schema(description = "修改时间")
+    @Schema(description = "更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     @Schema(description = "子项个数")
